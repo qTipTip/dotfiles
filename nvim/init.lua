7103cd918c2af090d7caa4b6996283fc7150fb0c
Ivar <istangeby@gmail.com>
2023-01-21 01:03:05 +0100

    Added lsp-zero

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,3 @@
 require('keybindings')
 require('plugin_management')
 require('colorscheme')
-
-vim.lsp.start({
-	name = "pylsp",
-	cmd = {"pylsp"},
-	root_dir = vim.fs.dirname(vim.fs.find({'setup.py', 'pyproject.toml'}, { upward = true })[1]),
-})
-
-
-require'lspconfig'.pylsp.setup{}
--- nvim/lua/plugin_management.lua
+++ nvim/lua/plugin_management.lua
@@ -19,11 +19,31 @@ require("lazy").setup(
 	"nvim-tree/nvim-web-devicons",
 	"neovim/nvim-lspconfig",
 	"nvim-lua/plenary.nvim",
-	{'ms-jpq/coq_nvim', branch = 'coq'},
-	{'ms-jpq/coq.artifacts', branch = 'artifacts'},
 	{'nvim-telescope/telescope.nvim', tag = "0.1.0", dependencies = {"nvim-lua/plenary.nvim"}},
 	"mbbill/undotree",
 	"tpope/vim-fugitive",
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		dependencies = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},         -- Required
+			{'hrsh7th/cmp-nvim-lsp'},     -- Required
+			{'hrsh7th/cmp-buffer'},       -- Optional
+			{'hrsh7th/cmp-path'},         -- Optional
+			{'saadparwaiz1/cmp_luasnip'}, -- Optional
+			{'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},             -- Required
+			{'rafamadriz/friendly-snippets'}, -- Optional
+		}
+	}
 }, 
 {}
 )
