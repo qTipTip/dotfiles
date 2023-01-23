febfe7b8b4c9e170b56429d7665707325f8b40c8
Ivar <istangeby@gmail.com>
2023-01-23 10:15:25 +0100

    Add files after restructure

---
+++ nvim/lua/qtiptip/init.lua
@@ -0,0 +1,4 @@
+require('qtiptip.set')
+require('qtiptip.keybindings')
+require('qtiptip.plugin_management')
+require('qtiptip.colorscheme')
---
+++ nvim/lua/qtiptip/keybindings.lua
@@ -0,0 +1,23 @@
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+vim.g.mapleader = " "
+map("i", "jk", "<esc>l", { silent = true })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move visual selection
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- append line below to current line without moving cursor
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>Y", "\"+Y")
