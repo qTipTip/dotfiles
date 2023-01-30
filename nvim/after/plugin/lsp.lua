5cde6aca07f1f06a43018b095ac63a6aca85d3a9
Ivar Stangeby <ivar.stangeby@cognite.com>
2023-01-30 07:46:44 +0100

    Removed somneo_lua specific config to get lsp to recognise vim api.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,8 +4,8 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.ensure_installed({ 'pylsp', 'sumneko_lua' })
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
+lsp.on_attach(function(client, buffer_number)
+    local opts = { buffer = buffer_number, remap = false }
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -13,22 +13,10 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     --vim.keymap.set("i", "<leader>s", function () vim.lsp.buf.signature_help() end, opts)
 end)
-
-lsp.configure(
-    'sumneko_lua', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-}
-)
 
 lsp.setup()
