cee92313b2e5812380a5e6419ea314a4879bc9f5
Ivar Stangeby <ivar.stangeby@cognite.com>
2023-01-30 10:19:09 +0100

    Remove needlessly complex map-func

--- nvim/lua/user/keybindings.lua
+++ nvim/lua/user/keybindings.lua
@@ -1,13 +1,5 @@
-function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
 vim.g.mapleader = " "
-map("i", "jk", "<esc>l", { silent = true })
+vim.keymap.set("i", "jk", "<esc>l", { silent = true })
 vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<cr>")
 
 -- move visual selection
