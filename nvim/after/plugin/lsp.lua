635286c70b41e0eb8e06905a06bbf51b82af91b8
Ivar Stangeby <ivar.stangeby@cognite.com>
2023-02-08 16:05:53 +0100

    Fixes

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -23,9 +23,11 @@ lsp.on_attach(function(client, buffer_number)
     local opts = { buffer = buffer_number, remap = false }
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float({
+    vim.keymap.set("n", "<leader>vd", function()
+        vim.diagnostic.open_float({
             { scope = "buffer" }
         })
     end, opts)
