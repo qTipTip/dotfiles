43f6a25ea3d7a19425b41c222b46fc8fd69dddcf
Ivar Stangeby <ivar.stangeby@cognite.com>
2023-02-27 15:00:45 +0100

    Add neorg

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -44,21 +44,6 @@ require("lazy").setup({
     {
         "nvim-neorg/neorg",
         build = ":Neorg sync-parsers",
-        opts = {
-            load = {
-                ["core.defaults"] = {}, -- Loads default behaviour
-                ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-                ["core.norg.dirman"] = { -- Manages Neorg workspaces
-                    config = {
-                        workspaces = {
-                            work = "~/notes/work",
-                            home = "~/notes/home"
-                        },
-                        default_workspace = "work"
-                    },
-                },
-            },
-        },
         dependencies = { { "nvim-lua/plenary.nvim" } },
     }
 }, {}
